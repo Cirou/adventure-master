9a0bac3394384e48915ea02440e5dbf53eeddc06
Ciro Aiello <ciroaiello@live.com>
2025-03-14 17:57:11 +0100

    Animations

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { HomeComponent } from './app/pages/home/home.component';
 import { AmbientazioneComponent } from './app/pages/ambientazione/ambientazione.component';
