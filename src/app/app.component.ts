b702e279a6508f274606cc6333a2deb7db7862a6
Ciro Aiello <ciroaiello@live.com>
2025-03-15 18:00:31 +0100

    Responsive NavBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,23 @@ import { Router, RouterModule, Event, NavigationEnd } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  menuOpen = false;
+
   constructor(private router: Router) {
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
         window.scrollTo({ top: 0, behavior: 'smooth' }); // ✅ Scroll fluido in cima alla pagina
       }
     });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
   }
 }
 
